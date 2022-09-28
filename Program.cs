64de1cbc63c4862607a997d616794293918a0386
notha <notha@LAPTOP-N9HV6Q5U>
2022-09-28 20:06:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multipleVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x , y ,z;
+             x = y = z = 50;
+            
+            Console.WriteLine( x + y + z);
+
+
+        }
+    }
+}
